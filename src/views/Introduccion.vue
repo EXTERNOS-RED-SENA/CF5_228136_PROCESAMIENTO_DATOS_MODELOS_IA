4d38bb0562e06971b97ef438fbec39abf0f5c4eb
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2024-11-20 19:52:46 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -34,7 +34,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/qas_rTn5Avs?si=1WKsYQzdPIQi00ds" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)     
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/qas_rTn5Avs?si=tsQG5P3PJob9-wO2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)     
 </template>
 
 <script>
